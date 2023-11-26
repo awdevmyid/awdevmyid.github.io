5be82fc8c7994569a082d3ec15797ae582148efa
awdevmyid <144774921+awdevmyid@users.noreply.github.com>
2023-11-26 20:05:30 +0700

    Update sample-config.js
    Signed-off-by: awdevmyid <144774921+awdevmyid@users.noreply.github.com>

--- app/public /sample-config.js
+++ app/public /sample-config.js
@@ -1,27 +1,30 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  databaseURL: "https://your-app.firebaseio.com",
-  storageBucket: "your-app.appspot.com",
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzIarG4tYrkA2xOOBx5hd66kdRLQ929Js",
+  authDomain: "awdevmyid.firebaseapp.com",
+  databaseURL: "https://awdevmyid-default-rtdb.firebaseio.com",
+  projectId: "awdevmyid",
+  storageBucket: "awdevmyid.appspot.com",
+  messagingSenderId: "1025289085908",
+  appId: "1:1025289085908:web:08228b4c81b0c215b9ac41",
+  measurementId: "G-D4FMFSEHC2"
 };
+
+// Initialize Firebase
 firebase.initializeApp(config);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 
 // Google OAuth Client ID, needed to support One-tap sign-up.
 // Set to null if One-tap sign-up is not supported.
 var CLIENT_ID =
-    'YOUR_OAUTH_CLIENT_ID';
+    '106457840177755049060';
