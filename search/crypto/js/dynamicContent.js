b540197eea4b8b06c8154c4ccc3cf8ac44a11d7e
awdevmyid <144774921+awdevmyid@users.noreply.github.com>
2023-11-27 14:33:04 +0700

    Update dynamicContent.js
    Signed-off-by: awdevmyid <144774921+awdevmyid@users.noreply.github.com>

--- search/crypto/js/dynamicContent.js
+++ search/crypto/js/dynamicContent.js
@@ -84,22 +84,7 @@ function AboutPage() {
         <div>CryptoCompare is the independent global cryptocurrency market data provider, giving institutional and retail investors access to real-time, high-quality, reliable market and pricing data. A central authority for clear and concise information, CryptoCompare offers unrivalled breadth, scope and depth of data, bridging the gap between the crypto asset and traditional financial markets.</div><br>
 <div>
 
-    <ul>
-          <li><a href="https://www.awdev.my.id/thema/"><i class="fas fa-qrcode"></i>Dashboard</a></li>
-          <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
-          <li><a href="https://www.awdev.eu.org"><i class="fas fa-stream"></i>Overview</a></li>
-          <li><a href="https://wahyu9kdl.github.io/islami"><i class="fas fa-calendar-week"></i>APP</a></li>
-          <li><a href="#"><i class="fas fa-question-circle"></i>About</a></li>
-          <li><a href="https://devoloper.awdev.eu.org"><i class="fas fa-sliders-h"></i>Services</a></li>
-          <li><a href="#"><i class="fas fa-phone-volume"></i>Contact</a></li>
-          <li><a href="#"><i class="fas fa-comments"></i>Feedback</a></li>
-        </ul>
-        <div class="media_icons">
-          <a href="https://web.facebook.com/Awfanspage"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://twitter.com/Awfanspage"><i class="fab fa-twitter"></i></a>
-          <a href="https://m.awdev.my.id"><i class="fab fa-instagram"></i></a>
-          <a href="https://youtube.com/channel/UCyp-Fn_0Ek4_aXIFbcWaq0w"><i class="fab fa-youtube"></i></a>
-        </div>
+
        <p>
         <div id="imgandauthor">
         
@@ -197,27 +182,7 @@ function AboutPage() {
         <li><a href="https://qr.awdev.my.id">QR ( Quick Response )</a><ul>
         <li><a href="https://qr.awdev.my.id/v3">View Web QR</a></li>
         </ul>
-        </li>
-        <li><a href="https://youtube.com/channel/UCyp-Fn_0Ek4_aXIFbcWaq0w">YOUTUBE</a><ul>
-        <li><a href="https://youtube.com/channel/UCyp-Fn_0Ek4_aXIFbcWaq0w">SUBCRIBE</a></li>
-        <li><a href="https://www.awdev.my.id/youtube/downloader.html">YOUTUBE DOWNLOADER</a></li>
-        </ul>
-        </li>
-        <li><a href="https://www.awdev.my.id/faq.html">FAQ</a></li>
-        <li><a href="https://www.awdev.my.id/privasy.html">PRIVASY</a></li>
-        <li><a href="https://www.awdev.my.id/404.html">404</a></li>
-        <li><a href="https://www.awdev.my.id/app/htmleditor-2-1.apk">APP</a><ul>
-        <li><a href="app/awdev-2-2.aab.zip">awdev-2-2.aab.zip</a></li>
-        <li><a href="app/awdev-2-2.apk">awdev-2-2.apk</a></li>
-        <li><a href="app/htmleditor-2-1.aab.zip">htmleditor-2-1.aab.zip</a></li>
-        <li><a href="app/htmleditor-2-1.apk">htmleditor-2-1.apk</a></li>
-        </ul>
-        </li>
-        <li><a href="LICENSE">License</a></li>
-        <li><a href="SECURITY.md">SECURITY</a></li>
-        <li><a href="#authors">Authors</a></li>
-        <li><a href="#acknowledgements">Acknowledgements</a></li>
-        </ul>
+        
         
         <!-- Adsense -->
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5295583488737876"
@@ -233,23 +198,16 @@ function AboutPage() {
         
         <!-- And Adsense -->
         </div>
+        <div
+  class="DePayButton"
+  title="Pay"
+  integration="882ccd43-679f-4311-859c-6f2752abea93"
+  blockchains='["ethereum"]'
+></div>
+<script src="https://integrate.depay.com/buttons/v12.js"></script>
+<noscript><a href="https://www.awdev.my.id">Web3 Payments</a> are only supported with JavaScript enabled.</noscript>
+<script>DePayButtons.init({document: document});</script>
         
-        <center>
-        <h3 align="left"><center><h1><span class="multicolortext" >Connect with me:</span></h3>
-        <p align="left">
-        <a href="https://codepen.io/san3" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/codepen.svg" alt="san3" height="30" width="40" /></a>
-        <a href="https://dev.to/wahyu9kdl" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/devto.svg" alt="wahyu9kdl" height="30" width="40" /></a>
-        <a href="https://twitter.com/Awfanspage" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="awfanspage" height="30" width="40" /></a>
-        <a href="https://www.linkedin.com/in/ahmad-wahyudi-41b6841b6" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="https://www.linkedin.com/in/ahmad-wahyudi-41b6841b6" height="30" width="40" /></a>
-        <a href="https://fb.com/awgroupchannel" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="awgroupchannel" height="30" width="40" /></a>
-        <a href="https://instagram.com/awfanspage" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="awfanspage" height="30" width="40" /></a>
-        <a href=" https://dribbble.com/wahyu9kdl/shots " target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/dribbble.svg" alt="wahyu9kdl" height="30" width="40" /></a>
-        <a href="https://wahyu9kdl.medium.com" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/medium.svg" alt="wahyu9kdl" height="30" width="40" /></a>
-        <a href="https://m.youtube.com/channel/UC7CRa3nkxakAZx_aRsMwRyA/playlists" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg" alt="aw youtube channel" height="30" width="40" /></a>
-        <a href="https://open.spotify.com/show/4UoPUn3e0VhJ6MGOwdsrHV" target="blank"> <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/rss.svg" alt="http://feedproxy.google.com/~r/blogspot/ghpgr/~3/vyr1fhc7dbi/informasi-cara-mendapatkan-ribuan.html" height="30" width="40" /></a>
-        <a href=" https://wahyu9kdl.github.io/wahyu9kdl" ><img align="center" src=" https://camo.githubusercontent.com/5b09aa408358eda7cba977c95dccaf65b070ed679c9be064297a11161058d1f2/68747470733a2f2f312e62702e626c6f6773706f742e636f6d2f2d735749774852616b54744d2f594a7068586a65796f46492f41414141414141414339632f37747230382d4d665f387379524457346c4c30514e7042564431773734772d6841434c63424741735948512f733630302f6c6f676f355f31315f31353330322e706e67 " height="30" width="40" /></a>		
-        <a href="https://www.alhikmah.my.id" ><img align="center" src="https://raw.githubusercontent.com/wahyu9kdl/wahyu9kdl/main/logo.png" height="30" width="40" /></a>
-        </p>
        </center>
     </div>
     `;
