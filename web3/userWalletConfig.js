7e171aea9f531741f83d5c8401118fd26e343ce3
awdevmyid <hasansaputramaulana@gmail.com>
2026-06-12 21:11:26 +0700

    Create userWalletConfig.js

---
+++ web3/userWalletConfig.js
@@ -0,0 +1,22 @@
+const userWalletConfig = {
+  eth: "0x5e8bade6e0bce65807db6327cb1d9eeb7c6a6a5b",
+  solana: "CB4xnv7Mw23eJkbRBXvbLUDUDQVQsg5AUvfY5nu8Ny5W",
+  btc: "bc1qfwn7lfntn5jyl9yxgcrnhu4gaz8w5mnua0acyt",
+  inj: "inj1t696mehqhnn9sp7mvvnuk8v7ad7x56jm5gslj6",
+  doge: "DSTbQeqpCajXu8BYmririjJ8b8Lx92Lvov",
+  ltc: "ltc1qz7ccrlkkv5qxrmlenr67jtqe3dy5k5w09sznvf",
+  fio: "FIO67SRBSYY42urrjvN11AWuuTWHQzo47jH8LE1nrthe48EBxDo9e",
+  icp: "8a92cb3db92beacc8982e1425c3c25b8f80381249d77ce19bf66c306d8727053",
+  iotx: "io1p7ej7yufhsc7fwqv5vngde5llse00a4zsrs83w",
+  zil: "zil1de4766z0f9mm9psyflk3tcrxc562cw068y863d",
+  egld: "erd1rgznaj8fvvy6jq80xyvnzjk0amv5t63l9vm276r0kf939gjy2p0sfakxj8",
+  osmo: "osmo148ds6u09ywv0d2zswjdcp0h9krs7f8eaf60gw5",
+  flux: "t1LKNhr6hitag9QwTk7sGgBnwyyQYxFqMbv",
+  juno: "juno148ds6u09ywv0d2zswjdcp0h9krs7f8eahnlrl6",
+  firo: "aE7PmZpWuW7aXBqCXhpGmaF2XLSYert4DR",
+  xrp: "rUT9m6CoZqdNWKFm7q2zo8Htsy1cvfSuk",
+  trx: "TBh1vo5wazVxz5FCqmkVZSDAG2RgXJRPbo"
+};
+
+// Contoh memanggil alamat ETH: userWalletConfig.eth
+console.log("Wallet ETH Terdaftar:", userWalletConfig.eth);
